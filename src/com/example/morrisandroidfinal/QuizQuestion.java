300550168bcdd50e691d92acc8f3c17fa097bd82
Nick <nickmorris@Nicks-MacBook-Pro-2.local>
2014-05-06 21:53:18 -0500

    Created QuizQuestion class

---
+++ src/com/example/morrisandroidfinal/QuizQuestion.java
@@ -0,0 +1,78 @@
+package com.example.morrisandroidfinal;
+
+public class QuizQuestion {
+	/*	Creating private attributes for QuizQuestion class.
+		Each question has 4 possible options for the user to select;
+		one is the correct answer, the other three are, of course, not.
+	 */
+	private int qID;
+	private String question;
+	private String opt1;
+	private String opt2;
+	private String opt3;
+	private String answer;
+	
+	//default constructor
+	public QuizQuestion() {
+		
+	}
+	
+	//main constructor
+	public QuizQuestion(String question, String num1, String num2, String num3, String correct) {
+		
+	}
+	
+	
+	//now a series of Accessor methods for the different attributes
+	
+	public int getID() {
+		return qID;
+	}
+	
+	public String getQuestion() {
+		return question;
+	}
+	
+	public String getFirstOpt() {
+		return opt1;
+	}
+	
+	public String getSecondOpt() {
+		return opt2;
+	}
+	
+	public String getThirdOpt() {
+		return opt3;
+	}
+	
+	public String getAnswer() {
+		return answer;
+	}
+	
+	//a series of Mutator methods next
+	
+	public void setID(int id) {
+		qID = id;
+	}
+	
+	public void setQuestion(String newQuestion) {
+		question = newQuestion;
+	}
+	
+	public void setFirstOpt(String firstOpt) {
+		opt1 = firstOpt;
+	}
+	
+	public void setSecondOpt(String secondOpt) {
+		opt2 = secondOpt;
+	}
+	
+	public void setThirdOpt(String thirdOpt) {
+		opt3 = thirdOpt;
+	}
+	
+	public void setAnswer(String ans) {
+		answer = ans;
+	}
+
+}
