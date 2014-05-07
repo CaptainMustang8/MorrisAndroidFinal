911789154497fdfd18376236ff311b4afba71f32
Nick <nickmorris@usr156-166.dyn.smu.edu>
2014-05-07 16:17:17 -0500

    Added a new background, changed more of the layout

--- gen/com/example/morrisandroidfinal/R.java
+++ gen/com/example/morrisandroidfinal/R.java
@@ -12,6 +12,8 @@ public final class R {
     }
     public static final class color {
         public static final int blue=0x7f060000;
+        public static final int red=0x7f060002;
+        public static final int white=0x7f060001;
     }
     public static final class dimen {
         /**  Default screen margins, per the Android Design guidelines. 
@@ -26,11 +28,14 @@ public final class R {
     }
     public static final class drawable {
         public static final int background=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
+        public static final int blue2=0x7f020001;
+        public static final int ic_launcher=0x7f020002;
+        public static final int mustangband=0x7f020003;
     }
     public static final class id {
-        public static final int action_settings=0x7f09000b;
+        public static final int action_settings=0x7f09000d;
         public static final int container=0x7f090000;
+        public static final int diamondM=0x7f09000c;
         public static final int next_question=0x7f09000a;
         public static final int option1=0x7f090006;
         public static final int option2=0x7f090007;
@@ -39,6 +44,7 @@ public final class R {
         public static final int options=0x7f090005;
         public static final int pony_background=0x7f090003;
         public static final int question=0x7f090004;
+        public static final int scorepage=0x7f09000b;
         public static final int title=0x7f090002;
         public static final int trivia_page=0x7f090001;
     }
