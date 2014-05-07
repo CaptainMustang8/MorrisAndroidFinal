db32b62a011c02ee39c83b38c5712456e09e69ff
Nick <nickmorris@usr156-166.dyn.smu.edu>
2014-05-07 18:41:34 -0500

    Added in developer name as string

--- gen/com/example/morrisandroidfinal/R.java
+++ gen/com/example/morrisandroidfinal/R.java
@@ -61,6 +61,7 @@ public final class R {
     public static final class string {
         public static final int action_settings=0x7f050001;
         public static final int app_name=0x7f050000;
+        public static final int developer_name=0x7f050003;
         public static final int title_activity_score=0x7f050002;
     }
     public static final class style {
